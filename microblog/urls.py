17c4acee79b4139b45e1669d68ff177efa6ec059
lpatmo <linda.peng@alumni.duke.edu>
2014-05-29 17:56:00 +0000

    added index.html and 500 files

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 # Uncomment the next two lines to enable the admin:
@@ -13,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r"^$", TemplateView.as_view(template_name="index.html")),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
